e6cfa67061da8fb3611a720f8fd01d7b4c584ecc
Michael Lisitsa <58811856+michaellisitsa@users.noreply.github.com>
2020-11-26 15:56:52 +1100

    Add toggle for reversing x and y axes for RHS's, ie turn them so wider than they are tall

--- validation.py
+++ validation.py
@@ -28,17 +28,25 @@ def hs_lookup(hs_type,member_type):
     if hs_type == "SHS":
         options = st.sidebar.selectbox("",shs,key=member_type)
         hs_chosen = shs[shs['Dimensions'] == options]
+        reverse_axes = False #member is symmetrical so reverse is not needed for SHS's
     elif hs_type == "RHS":
+        reverse_axes = st.sidebar.checkbox("Rotate 90 degrees W > H",key=member_type)
         options = st.sidebar.selectbox("",rhs,key=member_type)
         hs_chosen = rhs[rhs['Dimensions'] == options]
-    b = hs_chosen.iloc[0]['b'] / 1000
-    h = hs_chosen.iloc[0]['d'] / 1000
+    if reverse_axes:
+        b = hs_chosen.iloc[0]['d'] / 1000
+        h = hs_chosen.iloc[0]['b'] / 1000
+        I_x =  hs_chosen.iloc[0]['Iy'] / 1000**4
+        I_y =  hs_chosen.iloc[0]['Ix'] / 1000**4
+    else:
+        b = hs_chosen.iloc[0]['b'] / 1000
+        h = hs_chosen.iloc[0]['d'] / 1000
+        I_x =  hs_chosen.iloc[0]['Ix'] / 1000**4
+        try:
+            I_y =  hs_chosen.iloc[0]['Iy'] / 1000**4
+        except:
+            I_y = I_x #In RHS.csv I_y column does not exist, so needs to use I_x for SHS's
     t = hs_chosen.iloc[0]['t'] / 1000
     area = hs_chosen.iloc[0]['Area'] / 1000**2
-    I_x =  hs_chosen.iloc[0]['Ix'] / 1000**4
-    try:
-        I_y =  hs_chosen.iloc[0]['Iy'] / 1000**4
-    except:
-        I_y = I_x
     return b,h,t,area,I_x,I_y
     
